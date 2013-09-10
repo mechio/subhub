6032bd890cc6d011c02e7ad9605339a328cc004e
Barnaby Malet <bmalet@gmail.com>
2013-09-10 21:15:32 +0100

    update tooltip text on button

--- chrome/build/scripts/content.js
+++ chrome/build/scripts/content.js
@@ -3,7 +3,7 @@
 
   $('head').append("<style type=\"text/css\">\n  .sidebar-button.loading * {\n    display: none;\n  }\n  .sidebar-button.loading,\n  .sidebar-button.loading:hover,\n  .sidebar-button.loading:active {\n    background-image: url(https://github.global.ssl.fastly.net/images/spinners/octocat-spinner-16px.gif?8c695afe);\n    background-position: 50% 50%;\n    background-repeat: no-repeat;\n    height: 24px;\n    background-color: #EAEAEA;\n    box-shadow: none;\n    border: 1px solid #C5C5C5;\n  }\n</style>");
 
-  button = $("<a href=\"#\" class=\"minibutton sidebar-button\" title=\"Download this repository as a zip file\" rel=\"nofollow\">\n  <span class=\"octicon octicon-link-external\"></span><span>Open in Sublime</span>\n</a>");
+  button = $("<a href=\"#\" class=\"minibutton sidebar-button\" title=\"Open this repository in Sublime Text 3\" rel=\"nofollow\">\n  <span class=\"octicon octicon-link-external\"></span><span>Open in Sublime</span>\n</a>");
 
   $(".repository-sidebar .only-with-full-nav").append(button);
 
